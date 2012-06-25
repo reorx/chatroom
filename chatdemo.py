903710e037efe614fbfa421a2689750c8e475278
Reorx <novoreorx@gmail.com>
2012-06-25 12:37:15 +0800

    simple usable version 0.1

---
+++ chatdemo.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+import time
+import logging
+import tornado.auth
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import os.path
+import uuid
+
+# import zipdb
+
+# DB = zipdb.ZipDB('tornadochat.zipdb')
+
+from tornado.options import define, options
+
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/auth/login", AuthLoginHandler),
+            (r"/auth/logout", AuthLogoutHandler),
+            (r"/a/message/new", MessageNewHandler),
+            (r"/a/message/updates", MessageUpdatesHandler),
+        ]
+        settings = dict(
+            cookie_secret="43oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+            login_url="/auth/login",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            #xsrf_cookies=True,
+            autoescape="xhtml_escape",
+            debug=True
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    # db = DB
+
+    def get_current_user(self):
+        user_json = self.get_secure_cookie("user")
+        if not user_json:
+            return None
+        return tornado.escape.json_decode(user_json)
+
+
+class MainHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render("index.html", messages=MessageMixin.cache)
+
+
+class MessageMixin(object):
+    waiters = set()
+    cache = []
+    cache_size = 200
+    online_users = set()
+
+    def wait_for_messages(self, callback, cursor=None):
+        cls = MessageMixin
+        cls.online_users.add(self.current_user['username'])
+
+        if cursor:
+            index = 0
+            for i in xrange(len(cls.cache)):
+                index = len(cls.cache) - i - 1
+                if cls.cache[index]["id"] == cursor:
+                    break
+            recent = cls.cache[index + 1:]
+            if recent:
+                callback(recent)
+                return
+        cls.waiters.add(callback)
+
+    def cancel_wait(self, callback):
+        cls = MessageMixin
+        cls.online_users.remove(self.current_user['username'])
+        cls.waiters.remove(callback)
+
+    def new_messages(self, messages):
+        cls = MessageMixin
+        logging.info("Sending new message to %r listeners", len(cls.waiters))
+        for callback in cls.waiters:
+            try:
+                callback(messages)
+            except:
+                logging.error("Error in waiter callback", exc_info=True)
+        cls.waiters = set()
+        cls.cache.extend(messages)
+        if len(cls.cache) > self.cache_size:
+            cls.cache = cls.cache[-self.cache_size:]
+
+
+class MessageNewHandler(BaseHandler, MessageMixin):
+    @tornado.web.authenticated
+    def post(self):
+        message = {
+            "id": str(uuid.uuid4()),
+            "timestamp": int(time.time()),
+            "from": self.current_user["username"],
+            "body": self.get_argument("body"),
+        }
+        message["html"] = self.render_string("message.html", message=message)
+        if self.get_argument("next", None):
+            self.redirect(self.get_argument("next"))
+        else:
+            self.write(message)
+        self.new_messages([message])
+
+
+class MessageUpdatesHandler(BaseHandler, MessageMixin):
+    @tornado.web.authenticated
+    @tornado.web.asynchronous
+    def post(self):
+        cursor = self.get_argument("cursor", None)
+        self.wait_for_messages(self.on_new_messages,
+                               cursor=cursor)
+
+    def on_new_messages(self, messages):
+        # Closed client connection
+        if self.request.connection.stream.closed():
+            return
+        self.finish(dict(messages=messages))
+
+    def on_connection_close(self):
+        self.cancel_wait(self.on_new_messages)
+
+
+class AuthLoginHandler(BaseHandler):
+    def get(self):
+        self.render('login.html', message='Welcome')
+
+    def post(self):
+        username = self.get_argument('username')
+
+        if username in MessageMixin.online_users:
+            self.render('login.html', message='This username have been used')
+            return
+        # construct a dict for json encoding
+        user = {
+            'username': username,
+            'login_time': int(time.time())
+        }
+
+        self.set_secure_cookie("user", tornado.escape.json_encode(user))
+        self.redirect("/")
+
+
+class AuthLogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.write("You are now logged out")
+
+
+def main():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
